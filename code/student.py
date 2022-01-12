885c02c47b7076a1a6fc4f1dc645cf0c37a4b234
julialfk <j.liemfk@gmail.com>
2022-01-12 11:32:34 +0100

    Added student class and a loader function for students

---
+++ code/student.py
@@ -0,0 +1,7 @@
+class Student:
+
+    def __init__(self, last_name, first_name, student_number, courses):
+        self._last_name = last_name
+        self._first_name = first_name
+        self._student_number = student_number
+        self._courses = courses
