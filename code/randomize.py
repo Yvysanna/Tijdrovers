349011162f19e896d34737c12b2aaaffbc43eacf
julialfk <j.liemfk@gmail.com>
2022-01-12 14:14:36 +0100

    Added relationships in classes; Added counter for number of students per subject

--- code/randomize.py
+++ code/randomize.py
@@ -8,18 +8,21 @@ timeslots = ['9-11', '11-13', '13-15', '15-17']
 def randomize(subjects_list, schedule_dict):
     # create full list of activities
     activities_list = []
+    activities_dict = {}
 
     # for every activity of every subject, add activity to list
     for subject in subjects_list:
         for n in range(int(subject._min_timeslots)):
-            activities_list.append(subject._name + '_' + str(n))
+            activities_list.append(subject._name)
+            # activities_dict[activity] = subject._name
 
     # shuffle list of activities
     random.shuffle(activities_list)
+    print(activities_list)
 
     # add every activity to schedule
-    for i in schedule_dict:
-        for j in days:
-            for k in timeslots:
-                if len(activities_list) > 0:
-                    schedule_dict[i].at[k, j] = activities_list.pop(0)
+    #  for i in schedule_dict:
+    #     for j in days:
+    #         for k in timeslots:
+    #             if len(activities_list) > 0:
+    #                 schedule_dict[i].at[k, j] = activities_list.pop(0)
