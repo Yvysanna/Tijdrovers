023cdeb759f45dcffc2fc375ffd0ddd9ecd0ad42
Yvysanna <yvette.violetta@web.de>
2022-01-24 16:00:29 +0100

    Correction of maluspoint calculation

--- code/checker.py
+++ code/checker.py
@@ -13,6 +13,12 @@ from collections import Counter
 
 def count_points(course_set):
     malus = 0
+    # mal_dict = dict()
+    # mal_dict['free_period0'] = 0
+    # mal_dict['free_period1'] = 0
+    # mal_dict['free_period2'] = 0
+    # mal_dict['lack_capacity'] = 0
+    # mal_dict['conflicts'] = 0
 
     # Open schedule
     with open('data/results/results.csv', 'r', encoding="ISO-8859-1") as f:
@@ -41,7 +47,10 @@ def count_points(course_set):
                 # Add maluspoint if number of students exceed capacity
                     if len(activity._students_list) > activity._room.capacity:
                         malus += len(activity._students_list) - activity._room.capacity
+                        # mal_dict['lack_capacity'] +=1
 
+        
+        points = [0,1,3] # Maluspoint values
         # Check for timeslots in each day for each student
         for student in student_dict.values():
             for day in student:
@@ -53,14 +62,30 @@ def count_points(course_set):
                         for x in conflicts.values():
                             if x > 1:
                                 malus += (x - 1)
+                                # mal_dict['conflicts'] += 1
 
-                        # Add maluspoints for free periods
-                        if '9-11' in timeslots and '11-13' not in timeslots:
-                            if '13-15' in timeslots:
-                                malus += 1
-                            else:
-                                malus += 3
-                        elif '15-17' in timeslots and '13-15' not in timeslots:
-                            malus += 1
 
+                        # Calculate maluspoints for free periods
+                        check = [int(x.split('-')[0]) for x in conflicts.keys()]
+                        check.sort()
+                        #print(student, conflicts, '   ------   ',  check)
+
+                        # Check that more than one activity per day
+                        if len(check) > 1:
+                            l1 = check[:-1]
+                            l2 = check[1:]
+
+                            # Two times equal list but with shifted values to subtract one from the other
+                            zlip = zip(l1, l2)
+
+                            # Iterate over both lists to find difference between times
+                            for l1, l2 in zlip:
+                                idx = int(((l2 - l1) / 2) - 1) # Idx just because one break gives -1 and two give -3 (not 2)
+                                if idx > 2:
+                                    return False # If more than 2 breaks, invalid result, thus return early and stop
+
+                                #mal_dict[f'free_period{idx}'] += 1
+                                malus += points[idx]   
+
+    # print('checker', mal_dict)
     return malus
