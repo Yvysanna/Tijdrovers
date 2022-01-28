29da28a43003bd57b9a62685b400dd3e134f39c5
julialfk <j.liemfk@gmail.com>
2022-01-28 15:05:05 +0100

    Added a new file for the complete climber with new functions to switch activities in a schedule; Inserted the student switch functions into this file as well

--- code/algorithms/hill_climber.py
+++ code/algorithms/hill_climber.py
@@ -24,5 +24,96 @@ class HillClimber:
 
 
     def activity_switch(self):
-        # Pick a random activity
+        # Pick two random activities, making sure they are not the same activity
+        index_activity_1 = randrange(0, len(self.planner.slots))
+        random_activity_1 = self.planner.slots[index_activity_1]
+        while True:
+            index_activity_2 = randrange(0, len(self.planner.slots))
+            random_activity_2 = self.planner.slots[index_activity_2]
+            if random_activity_1 != random_activity_2:
+                break
         
+        # Switch the position of the activities in the schedule
+        self.planner.slots[index_activity_1] = random_activity_2
+        self.planner.slots[index_activity_2] = random_activity_1
+
+        return(index_activity_1, index_activity_2)
+
+
+    def undo_activity_switch(self, index_activity_1, index_activity_2):
+        # Switch activities back to previous state
+        previous_activity_2 = self.planner.slots[index_activity_1]
+        previous_activity_1 = self.planner.slots[index_activity_2]
+
+        self.planner.slots[index_activity_1] = previous_activity_1
+        self.planner.slots[index_activity_2] = previous_activity_2
+
+
+    def reassign(self):
+        # Pick a random tutorial or lab
+        possible_activities = []
+        possible_tutorials = []
+        possible_labs = []
+        while len(possible_activities) == 0:
+            random_course = choice(self._courses)
+            # Only take activities with multiple groups
+            if len(random_course._tutorials) > 1:
+                possible_tutorials = random_course._tutorials
+                possible_activities.extend(random_course._tutorials)
+
+            if len(random_course._labs) > 1:
+                possible_labs = random_course._labs
+                possible_activities.extend(random_course._labs)
+
+        # Pick a random group and determine which type of activity the next group needs to be
+        random_group_1 = choice(possible_activities)
+        if random_group_1._type == 'Tutorial':
+            possible_activities = possible_tutorials
+        elif random_group_1._type == 'Lab':
+            possible_activities = possible_labs
+        else:
+            raise Exception("Group typing incorrect or could not copy activities")
+
+        # Remove group 1 from possible groups to switch with
+        possible_activities.remove(random_group_1)
+
+        # Pick group 2 and a student from group 1 and move the student to group 2
+        random_group_2 = choice(possible_activities)
+        random_student_1 = choice(random_group_1._students_list)
+        random_student_2 = None
+
+        self.reassign(random_student_1, random_group_1, random_group_2)
+
+        # If group 2 was already full, move a random student from group 2 to group 1
+        if random_group_2._max_capacity <= len(random_group_2._students_list):
+            random_student_2 = choice(random_group_2._students_list)
+
+            self.reassign(random_student_1, random_group_1, random_group_2)
+            self.reassign(random_student_2, random_group_2, random_group_1)
+
+        assert len(random_group_1) < random_group_1._max_capacity, f"Too many students in {random_group_1}"
+        assert len(random_group_2) < random_group_2._max_capacity, f"Too many students in {random_group_2}"
+
+        return random_group_1, random_group_2, random_student_1, random_student_2
+
+
+    def undo_reassign(self, random_group_1, random_group_2, random_student_1, random_student_2):
+        # Undo reassignment
+        self.student_switch(random_student_1, random_group_2, random_group_1)
+        if random_student_2 != None:
+            self.student_switch(random_student_2, random_group_1, random_group_2)
+
+        assert len(random_group_1) < random_group_1._max_capacity, f"Too many students in {random_group_1}"
+        assert len(random_group_2) < random_group_2._max_capacity, f"Too many students in {random_group_2}"
+
+
+    def student_switch(self, student, current_group, new_group):
+        
+        current_group._students_list.remove(student)
+        new_group._students_list.append(student)
+
+        student.activities.remove(current_group)
+        student.activities.add(new_group)
+
+
+    
