3bfc439cec508fe33604cd069ea8bb7db0f70412
5inu <nijhuis.karelarjan@gmail.com>
2022-01-31 19:12:13 +0100

    Added annealing to climbers

--- code/store.py
+++ code/store.py
@@ -18,10 +18,10 @@ def store(students_set, planner, min_points):
                 df_dict['activiteit'].append(activity._type)
                 df_dict['zaal'].append(room.name)
                 df_dict['dag'].append(day or 'tba')
-                df_dict['tijdslot'].append(time or 'tba') 
+                df_dict['tijdslot'].append(time or 'tba')
 
 
     results_df = pd.DataFrame.from_dict(df_dict, orient='columns', dtype=None, columns=None)
-    with open(f'data/climber{min_points}.csv', 'w+'):
+    with open(f'data/results/climber{min_points}.csv', 'w+'):
         pass
     results_df.to_csv(f'data/climber{min_points}.csv', sep = ';', index=False)
