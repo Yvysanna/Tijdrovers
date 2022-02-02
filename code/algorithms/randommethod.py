ba9ece7b7469e7f44b6f83eed39ad6ed4a076e8d
5inu <nijhuis.karelarjan@gmail.com>
2022-02-02 17:39:45 +0100

    Moved conflicts.py, cleaned up random and semirandom

--- code/algorithms/randommethod.py
+++ code/algorithms/randommethod.py
@@ -1,25 +1,28 @@
-import random
+# ==================================================================================
+# randommethod.py
+#
+# Julia Liem, Karel Nijhuis, Yvette Schröder
+#
+# - Random algorithm to create a class schedule
+# - Adds every activity to a random timeslot in a random classroom
+# ==================================================================================
+
+from random import shuffle, choice
 
 def random_method(course_set, classrooms_list, planner, days, timeslots):
-    """
-    Completely random method / method to begin from
-    Adds every activity to a random timeslot in a random room
-    RETURNS: None
-    """
-    all_activities_global = []
+    # Add activities to list and randomly shuffle
+    activity_list = []
     for course in course_set:
         all_activities = course.lectures + course.tutorials + course.labs
-
         for activity in all_activities:
-            all_activities_global.append(activity)
+            activity_list.append(activity)
+    shuffle(activity_list)
 
-    random.shuffle(all_activities_global)
-
-
-    for activity in all_activities_global:
+    # Plan activity in schedule for a random classroom in a random timeslot on a random day if not occupied
+    for activity in activity_list:
         while planner.get_info(activity) == (None, None, None):
-            room = random.choice(classrooms_list)
-            day = random.choice(days)
-            time = random.choice (timeslots)
+            room = choice(classrooms_list)
+            day = choice(days)
+            time = choice (timeslots)
             planner.insert_activity(activity, room, day, time)
             activity.room = room
--- code/loader.py
+++ code/loader.py
@@ -5,8 +5,9 @@
 #
 # - Loads in data from csv files
 # - Initializes and connects objects
-# 
-# Functions: load_classrooms, load_courses, load_students, load_activities, load_results, load_all 
+#
+# Functions: load_classrooms, load_courses, load_students, load_activities,
+#            load_results, load_all
 # ==================================================================================
 
 import csv
@@ -19,10 +20,8 @@ from objects.activity import Activity
 
 def load_classrooms():
     """
-    ARGS:
-        None
-    USAGE:
-        Loads all classrooms from csv file
+    Loads all classrooms from csv file
+
     RETURNS:
         Sorted list of all classroom objects sorted by classroom capacity ascending
     """
@@ -38,16 +37,15 @@ def load_classrooms():
     # Return list of objects sorted by capacity ascending
     return sorted(classrooms_list, key=lambda c: c.capacity, reverse=False)
 
- 
+
 def load_courses():
     """
-    ARGS:
-        None
-    USAGE:
-        Loads all courses from csv file
+    Loads all courses from csv file
+
     RETURNS:
         course_set: Set of all course objects
     """
+
     course_set = set()
 
     with open('data/courses.csv', 'r') as f:
@@ -56,7 +54,7 @@ def load_courses():
 
         # Create course object from course data and add it to the set of all objects
         for row in reader:
-            course = Course(row[0], row[1], row[2], row[3], row[4], row[5])              
+            course = Course(row[0], row[1], row[2], row[3], row[4], row[5])
             course_set.add(course)
 
     return course_set
@@ -64,14 +62,15 @@ def load_courses():
 
 def load_students(course_set):
     """
+    Loads all students from csv file
+    Creates student objects and connects them with the according course objects
+
     ARGS:
         course_set: set of all course objects
-    USAGE:
-        Loads all students from csv file
-        Creates student objects and connects them with the according course objects
     RETURNS:
         students_set: set of student objects
     """
+
     students_set = set()
 
     # Open csv
@@ -87,23 +86,22 @@ def load_students(course_set):
             # Iterate over all courses student is registered to
             for course in row[3:]:
                 if course:
-                    
+
                     # Find course object through equivalent string, connect student object with course object
                     course_object = [c for c in course_set if c.name == course][0]
                     course_object.add_student(student)
 
-    return students_set  
+    return students_set 
 
 
 def load_activities(course_set, classrooms_list):
     """
+    Creates activity objects through courses and randomly registers students into it
+
     ARGS:
         course_set, classrooms_list
-    USAGE:
-        Creates activity objects through courses and randomly registers students into it
-    RETURNS:
-        None
     """
+
     for course in course_set:
         course.create_activities(classrooms_list)
         course.random_register()
@@ -111,13 +109,14 @@ def load_activities(course_set, classrooms_list):
 
 def load_results(classrooms_list = None):
     """
+    Loads results from results.csv for maluspoint calculation
+
     ARGS:
         optional: classrooms_list
-    USAGE:
-        Loads results from results.csv for maluspoint calculation
-    RETURNS: 
-        List of activities, students dictionary {Student : [{Day : [Timeslot]}]}
+    RETURNS:
+        List of activities, students dictionary with format {Student : [{Day : [Timeslot]}]}
     """
+
     file = 'data/climber64.csv'
 
     # Create list with classroom objects if not given as argument
@@ -170,32 +169,17 @@ def load_results(classrooms_list = None):
 
 def load_all():
     """
-    ARGS: 
-        None 
-    USAGE:
-        Loads all necessary objects and creates all necessary class objects
-    RETURNS: 
+    Loads all data and creates every object for the program
+
+    RETURNS:
         classrooms_list : List of all classroom objects sorted after capacity ascending
         students_set : Set of all student objects
         course_set : Set of all course objects
     """
+
     classrooms_list = load_classrooms()
     course_set = load_courses()
     students_set = load_students(course_set)
-    
     load_activities(course_set, classrooms_list)
+
     return classrooms_list, students_set, course_set
-
-if __name__ == '__main__':
-    #import time
-    # classrooms_list, students_set, course_set= loadall()
-    # for course in course_set:
-    from checker import checker
-    print(checker(*load_results()))
-
-    # t0 = time.time()
-    # for i in range(1000000):
-    #     load_classrooms()
-    # t1 = time.time()
-    # total = t1-t0
-    # print(t0, t1, total)
