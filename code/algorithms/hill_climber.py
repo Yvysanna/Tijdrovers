af01125a7442b816fdb61978c8be549f8f368ff8
Yvysanna <yvette.violetta@web.de>
2022-02-03 01:08:06 +0100

    import switched to top line

--- code/algorithms/hill_climber.py
+++ code/algorithms/hill_climber.py
@@ -1,3 +1,6 @@
+from checker import checker
+from random import choice, random, sample
+
 import sys
 import os
 
@@ -5,9 +8,6 @@ current = os.path.dirname(os.path.realpath(__file__))
 parent = os.path.dirname(current)
 sys.path.append(parent)
 sys.setrecursionlimit(10000)
-
-from checker import checker
-from random import choice, random, sample
 
 
 class HillClimber:
